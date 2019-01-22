aaa527158e85bfd215b9ef00b9ddc0cda8f13ff3
Jacques DeBar <jacques.debar@gmail.com>
2019-01-21 19:18:11 -0800

    define person toString

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -166,7 +166,7 @@ open class Person {
   }
   
   open func toString() -> String {
-    return "[Person: firstName: \(self.firstName) lastName: \(self.lastName) age: \(self.age) job: \(self.job) spouse: \(self.spouse)]"
+    return "[Person: firstName: \(self.firstName) lastName: \(self.lastName) age: \(self.age) job: \(self.job?.type) spouse: \(self.spouse?.firstName)]"
   }
 }
 
